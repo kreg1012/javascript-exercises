002b66e6850f51484c03bd248ad9ab9c6aa9421e
kreg1012 <kreg1012@gmail.com>
2022-11-30 16:32:51 -0500

    testing palindromes. Getting false returns. v1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let filter = /[\W_]/g;
+    let filteredStr = str.toLowerCase().replace(filter, "");
+    let revStr = filteredStr.split("").reverse().join("");
+
+    return revStr === str ? true : false;
+    console.log(revStr);
 
 };
 
