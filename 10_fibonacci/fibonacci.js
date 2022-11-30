b39d637ddee7ac15486c72ad2e96e25e01b3f47b
kreg1012 <kreg1012@gmail.com>
2022-11-30 18:11:55 -0500

    error capture complete. Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    const fib = [0,1];
+
+    if (n < 0) return "OOPS";
+    if (typeof n === 'string'){
+        n = parseInt(n);
+        if (n < 0 || Number.isInteger(n) == false) return "OOPS";
+    } 
+    if (n == 1 || n == 2){
+        return 1;
+    }
+    else if (n > 2){
+        for (i = 1; i < n; i++){
+            fib.push(fib[fib.length-1] + fib[fib.length-2]);
+        }
+    }
+    return fib[n];
+    
 };
 
 // Do not edit below this line
