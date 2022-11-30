c73cb89513ac71bf0a193d5e59ede74871099649
kreg1012 <kreg1012@gmail.com>
2022-11-30 14:33:29 -0500

    stuck at factorial function.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,20 +3,26 @@ const add = function(a,b) {
   
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return subNum = a-b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((x,y) => x + y, 0);
 };
 
-const multiply = function() {
+const multiply = function(args) {
+  return args.reduce((last,next) => last * next);
 
 };
 
-const power = function() {
-	
+const power = function(start, exp) {
+  if (start === 0) return 1;
+  let final = 1;
+  for (i = 0; i < exp; i++){
+     final *= start;
+  }
+  return final;
 };
 
 const factorial = function() {
